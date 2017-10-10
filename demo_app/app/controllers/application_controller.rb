e71d3eb3301a48e7f5fbefd0e5d949a9f8f71497
MihaelaGadzhalova707 <mihaelagadzhalova@gmail.com>
2017-10-10 23:48:11 +0300

    all

--- demo_app/app/controllers/application_controller.rb
+++ demo_app/app/controllers/application_controller.rb
@@ -1,6 +1,7 @@
 class ApplicationController < ActionController::Base
 	protect_from_forgery with: :null_sessions
 	require 'csv'
+	require 'linefit'
 
 	def sums
 		
@@ -24,8 +25,44 @@ class ApplicationController < ActionController::Base
 		render plain: "%.2f" % sum.ceil
 		
 	end
-
 	
+	def intervals 
+        	file = params["file"].read
+        	arr = CSV.parse(file, converters: :numeric)
+
+        	maxsum = 0
+        	for  i in 0..arr.length - 30
+            	sum = 0
+            		for  p in  0.. 29
+               	        sum += arr[p+i][0]
+              	end
+            	if maxsum < sum 
+               		 maxsum = sum
+           	 end
+        end
+        render plain: "%.2f" % max_sum.ceil 
+    end
+	
+
+	def lin
+    	x = []
+    	y = []
+
+    	nope = params[:file]
+
+    	CSV.foreach(nope.path) do |row|
+      	x.push($INPUT_LINE_NUMBER)
+      	y.push(row[0].to_f)
+   	 end
+
+    	lineFit = LineFit.new
+
+    	lineFit.setData(x, y)
+
+    	intercept, slope = lineFit.coefficients
+
+    	render :html => '%.6f,%.6f' % [slope, intercept]
+  end	
 end
 
 
